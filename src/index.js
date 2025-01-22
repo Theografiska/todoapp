a0f4ad509aaeffb45358eb3cb4e4e1ad6c753215
Theodor Kaljo <theodor.kaljo@gmail.com>
2025-01-22 22:42:20 +0200

    Fix: Remove issue with confirmBtn event listener duplication

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import todoCreate from "./todoCreate.js";
 import todoRender from "./todoRender.js";
 
 // sample task
-const newTodo = new Todo("Pick up trash", "Just do it", "31 Jan 2025", "High", "Not started", ["Home", "Away"]);
+/* const newTodo = new Todo("Pick up trash", "Just do it", "31 Jan 2025", "High", "Not started", ["Home", "Away"]);
 todoRender(newTodo);
 
-console.log(newTodo);
+console.log(newTodo); */
 
 const addTaskButton = document.querySelector("#add-task-btn");
 addTaskButton.addEventListener("click", todoCreate);
--- src/todoCreate.js
+++ src/todoCreate.js
@@ -2,11 +2,15 @@ import Todo from "./todoClass.js";
 import todoRender from "./todoRender.js";
 
 const todoCreate = () => {
-    const taskSection = document.querySelector("#task-section");
-
     // opening up the dialog to get input for tasks
     const dialog = document.querySelector("dialog");
     dialog.showModal();
+
+    // resetting the form
+    const inputClass = document.querySelectorAll(".input-class");
+    inputClass.forEach((item) => {
+        item.value = "";
+    })
 
     // close button to close the dialog
     const closeButton = document.querySelector("#close-btn");
@@ -16,26 +20,34 @@ const todoCreate = () => {
 
     // submiting the dialog
     const confirmBtn = document.querySelector("#confirm-btn");
-    confirmBtn.addEventListener("click", (event) => {
-        // capturing user imputs from dialog
-        const newTitle = document.querySelector("#title-input").value;
-        const newDescription = document.querySelector("#description-input").value;
-        const newDueDate = document.querySelector("#due-date-input").value;
-        const newPriority = document.querySelector("#priority-input").value;
-        const newStatus = document.querySelector("#status-input").value;
-        const newLabels = document.querySelector("#status-input").value;
 
-        // creating a new task or todo
-        const newTask = new Todo(newTitle, newDescription, newDueDate, newPriority, newStatus, newLabels);
-        console.log(newTask);
-
-        todoRender(newTask); 
-
-        // preventing information sending event
-        event.preventDefault(); 
-        dialog.close();
-    })
+    if (!confirmBtn.dataset.listener) { // Checks if the listener already exists to avoid duplicates
+        confirmBtn.addEventListener("click", (event) => {
+            // preventing information sending event
+            event.preventDefault(); 
     
+            // capturing user inputs from dialog
+            const newTitle = document.querySelector("#title-input").value;
+            const newDescription = document.querySelector("#description-input").value;
+            const newDueDate = document.querySelector("#due-date-input").value;
+            const newPriority = document.querySelector("#priority-input").value;
+            const newStatus = document.querySelector("#status-input").value;
+            const newLabels = document.querySelector("#labels-input").value;
+    
+            // creating a new task or todo
+            const newTask = new Todo(newTitle, newDescription, newDueDate, newPriority, newStatus, newLabels);
+            console.log(newTask);
+            
+            // rendering the new task
+            todoRender(newTask); 
+
+            // closing the dialog
+            dialog.close();
+        });
+
+        // Mark the listener as added
+        confirmBtn.dataset.listener = "true";
+    }
 }
 
 export default todoCreate;
